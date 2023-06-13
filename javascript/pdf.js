f8abceb3b772bf1e7eb6a89c4b526808b1b47369
Subodh Vashistha <43090551+vvashu38@users.noreply.github.com>
2023-06-13 18:23:18 +0530

    Update pdf.js

--- javascript/pdf.js
+++ javascript/pdf.js
@@ -1,7 +1,15 @@
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
+function openModal(event) {
+    event.preventDefault(); // Prevents the default behavior of the link
+
+    var pdfUrl = event.target.href; // Get the URL of the PDF from the link's href attribute
+    var pdfEmbed = document.getElementById("pdfEmbed"); // Get the <embed> element
+
+    pdfEmbed.setAttribute("src", pdfUrl); // Set the PDF URL as the src attribute of the <embed> element
+
+    document.getElementById("myModal").style.display = "block"; // Show the modal
   }
 
   function closeModal() {
-    document.getElementById("myModal").style.display = "none";
+    document.getElementById("myModal").style.display = "none"; // Hide the modal
+    document.getElementById("pdfEmbed").setAttribute("src", ""); // Clear the src attribute of the <embed> element
   }
